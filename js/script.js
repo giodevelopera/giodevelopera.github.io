cb8ed5d93fb2fab3a7b3869f302eb3e0d96aee95
Giorgi Mamisashvili <giomamisashvili25@gmail.com>
2023-11-10 19:41:54 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+
+
+const burger_nav = document.getElementById("mobile-menu");
+const burger_icon = document.getElementById("burger");
+const menu_close = document.getElementById("menu-close");
+
+burger_icon.addEventListener("click", () => {
+  burger_nav.classList.add("close");
+});
+
+menu_close.addEventListener("click", () => {
+  burger_nav.classList.remove("close");
+});
